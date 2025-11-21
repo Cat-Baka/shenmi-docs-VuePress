0b7f6eabbd4eafd0e975d54375def9215487fbda
Cat-Baka <astgca@163.com>
2025-11-21 21:41:59 +0800

    1

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://catbaka.cn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -39,7 +39,7 @@ const config = {
     defaultLocale: 'zh-Hans',
     locales: ['zh-Hans'],
   },
-
+  
   presets: [
     [
       'classic',
