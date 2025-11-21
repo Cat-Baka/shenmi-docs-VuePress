91fc8f1cdd0b2c0182a44b8aa334df0226048200
Cat-Baka <astgca@163.com>
2025-11-21 21:33:45 +0800

    1

--- vuepress/docs/.vuepress/.temp/pages/index.html.vue
+++
@@ -1,5 +0,0 @@
-<template><div><h1 id="hello-world" tabindex="-1"><a class="header-anchor" href="#hello-world"><span>hello world</span></a></h1>
-<h1 id="你好世界" tabindex="-1"><a class="header-anchor" href="#你好世界"><span>你好世界</span></a></h1>
-</div></template>
-
-
